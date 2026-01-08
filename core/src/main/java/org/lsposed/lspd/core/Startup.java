2b83627b440c8c3c7b5e1b93c7485593d1136898
李明阳 <limingyang.147@bytedance.com>
2026-01-08 15:55:57 +0800

    减少检测点

--- core/src/main/java/org/lsposed/lspd/core/Startup.java
+++ core/src/main/java/org/lsposed/lspd/core/Startup.java
@@ -48,7 +48,7 @@ import de.robv.android.xposed.XposedInit;
 public class Startup {
     private static void startBootstrapHook(boolean isSystem) {
         Utils.logD("startBootstrapHook starts: isSystem = " + isSystem);
-        LSPosedHelper.hookMethod(CrashDumpHooker.class, Thread.class, "dispatchUncaughtException", Throwable.class);
+        //LSPosedHelper.hookMethod(CrashDumpHooker.class, Thread.class, "dispatchUncaughtException", Throwable.class);
         if (isSystem) {
             LSPosedHelper.hookAllMethods(HandleSystemServerProcessHooker.class, ZygoteInit.class, "handleSystemServerProcess");
         } else {
